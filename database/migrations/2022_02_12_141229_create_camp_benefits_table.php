e69e0a6086998b06fc21258420161bde5ff1c677
Muhammad <mfakhrizm18@gmail.com>
2022-03-07 20:01:21 +0700

    finish migration Camp, Camp_benefit, Checkout

---
+++ database/migrations/2022_02_12_141229_create_camp_benefits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('camp_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
